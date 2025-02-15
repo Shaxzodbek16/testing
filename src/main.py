772fb8a055814510a7300c95435e3964728b984a
Shaxzodbek <muxtorovshaxzodbek16@gmail.com>
2025-02-15 09:40:31 +0500

    test sum_ added

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 class Logic:
     logic_param = 10
 
+
 def func(x):
-    return x+1
+    return x + 1
+
+
+def sum_(x, y):
+    return x + y
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,29 @@
-from src.main import Logic
+from src.main import Logic, sum_
+import pytest
 
 from src.main import func
 
+
 def test_main():
+
     assert 1 == 1
+
 
 def test_logic():
     logic = Logic()
+
     assert logic.logic_param == 10
 
 
 def test_func():
+
     assert func(1) == 2
 
+
+def test_sum():
+
+    assert sum_(1, 1) == 2
+
+
+def test__sum_error():
+    pytest.raises(TypeError, sum_, 1, "1")
