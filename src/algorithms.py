2cd4fbf3f208b76a9d3fac91d5bf25e741835cfc
Shaxzodbek <muxtorovshaxzodbek16@gmail.com>
2025-02-18 10:19:12 +0500

    algorithms added with testcases

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def find_closest_number(nums):
     closest = nums[0]
     for el in nums:
@@ -8,3 +11,15 @@ def find_closest_number(nums):
         return abs(closest)
     else:
         return closest
+
+
+def contains_duplicat(nums: List[int]) -> bool:
+    if not isinstance(nums, list):
+        raise TypeError("Input should be a list")
+    seen = set()
+    for i in nums:
+        if i in seen:
+            return True
+        else:
+            seen.add(i)
+    return False
--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -1,5 +1,6 @@
-from src.algorithms import find_closest_number
+from src.algorithms import find_closest_number, contains_duplicat
 import pytest
+from contextlib import nullcontext as does_not_raise
 
 
 @pytest.mark.parametrize(
@@ -27,3 +28,32 @@ import pytest
 )
 def test_find_numbers_closest_to_zero(numbers, expected):
     assert find_closest_number(numbers) == expected
+
+
+@pytest.mark.parametrize(
+    "numbers, expected, expectation",
+    [
+        ([1, 2, 3], False, does_not_raise()),
+        ([-1, -2, -3], False, does_not_raise()),
+        ([2, 3, -1, 1, -2, -3], False, does_not_raise()),
+        ([1, 2, 3, -1, -2, -3, 0], False, does_not_raise()),
+        ([1, 2, 3, -1, -2, -3, 0, 0], True, does_not_raise()),
+        ([1, 2, 3, -1, -2, -3, 0, 0, 0], True, does_not_raise()),
+        ([1, 2, 3, -1, -2, -3, 0, 0, 0, 0], True, does_not_raise()),
+        ([1, 2, 3, -1, -2, -3, 0, 0, 0, 0, 0], True, does_not_raise()),
+        ([-1000, -1000], True, does_not_raise()),
+        ([42], False, does_not_raise()),
+        ([-7], False, does_not_raise()),
+        ([-5, 5], False, does_not_raise()),
+        ([1000, -1000], False, does_not_raise()),
+        ([10, -10, 5, -2, 2, 1, 0], False, does_not_raise()),
+        ([0, 0, 5, -5, 2, -2], True, does_not_raise()),
+        ([3, 3, -3, -2, -2, 2, 2, 1, 1], True, does_not_raise()),
+        ([999999999, -1000000000, 500, -500], False, does_not_raise()),
+        ([100, 50, 25, -3, 3, 10, 20], False, does_not_raise()),
+        ("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1]", True, pytest.raises(TypeError)),
+    ],
+)
+def test_contains_duplicate(numbers, expected, expectation):
+    with expectation:
+        assert contains_duplicat(numbers) == expected
