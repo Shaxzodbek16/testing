d41a9716eb9428d3133b52c0c658a0d44f80053f
Shaxzodbek <muxtorovshaxzodbek16@gmail.com>
2025-02-25 10:28:16 +0500

    fixture and session function were learned how to use them

---
+++ tests/test_fixture_auto_use.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+@pytest.fixture(scope="session", autouse=True)
+def mock_user_data():
+    print("\nSession scope fixture was opened")
+    users = {"id": 1, "username": "alice", "email": "alica@gmail.com", "age": 15}
+
+    yield users
+
+    print("\nSession scope fixture was closed")
+
+
+def test_user_data_1(mock_user_data):
+    assert mock_user_data["id"] == 1
+    assert mock_user_data["username"] == "alice"
+
+
+def test_user_data_2(mock_user_data):
+    assert mock_user_data["email"] == "alica@gmail.com"
+    assert mock_user_data["age"] == 15
